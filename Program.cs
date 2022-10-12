45d066bc7e3a7091b05e02020eb42a7878a5c208
Anh Nguyen <anguyen@Nguyen-MacBook-Pro.local>
2022-10-12 12:57:10 +1100

    working with three routes

---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => AddTwoIntegers(2, 5));
+
+// map a new get route to /add
+app.MapGet("/add", () => AddTwoIntegers(9, 8));
+
+// parameter routing
+app.MapGet("/add/{input1}/{input2}", (int input1, int input2) => AddTwoIntegers(input1, input2));
+
+
+
+app.Run();
+
+// function to add two integers together
+int AddTwoIntegers(int num1, int num2) {
+    int ans = num1 + num2;
+    return ans;
+}
